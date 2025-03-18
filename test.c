6e525a6f65edb9e1abafdc9c81b98b278ce33344
Elea Krzewinski <elea.krzewinski.etu@univ-lille.fr>
2025-03-18 16:35:08 +0100

    gestion aucune reponse valable

--- test.c
+++ test.c
@@ -16,6 +16,16 @@ get_input(const char *prompt, char *buffer, size_t size) {
     buffer[strcspn(buffer, "\n")] = 0;  // Supprime le retour à la ligne
 }
 
+void affichage(char * schema,char * table){
+    printf("(SGDB) ");
+    if(strcmp(schema,"")!=0){
+        printf("%s> ",schema);
+    }
+    if(strcmp(table,"")!=0){
+        printf("%s> ",table);
+    }
+}
+
 int 
 main() {
     char host[MAX_INPUT], dbname[MAX_INPUT], user[MAX_INPUT], password[MAX_INPUT];
@@ -39,7 +49,7 @@ main() {
     printf("Connexion réussie à la base de données !\n");
 
     while (1) {
-        printf("(SGDB) %s:%s : ", schema, table);
+        affichage(schema,table);
         char cmd[256];
         fgets(cmd, sizeof(cmd), stdin);
         if (strcmp(cmd, "help\n") == 0){
@@ -48,12 +58,17 @@ main() {
         if (strcmp(cmd, "list schema\n") == 0) {
             char * exec = list_schema();
             PGresult *res = PQexec(conn, exec);
-            if (PQresultStatus(res) != PGRES_TUPLES_OK) {
+            if (PQresultStatus(res) == PGRES_EMPTY_QUERY) {
                 fprintf(stderr, "Requête échouée : %s\n", PQerrorMessage(conn));
                 PQclear(res);
                 continue;
             }
             int n = PQntuples(res);
+            if(n==0){
+                printf("Erreur dans la requete\n");
+                PQclear(res);
+                continue;
+            }
             for (int i = 0; i < n; i++) {
                 printf("%s\n", PQgetvalue(res, i, 0));
             }
@@ -75,6 +90,11 @@ main() {
                 continue;
             }
             int n = PQntuples(res);
+            if(n==0){
+                printf("Erreur dans la requete\n");
+                PQclear(res);
+                continue;
+            }
             for (int i = 0; i < n; i++) {
                 printf("%s\n", PQgetvalue(res, i, 0));
             }
